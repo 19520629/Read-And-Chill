11b48405c9f5cdcb049661d7c471cb42249cf8e1
anhkhoalongha <67829232+19520629@users.noreply.github.com>
2021-11-02 20:41:32 +0700

    form dang ki

--- read_app/urls.py
+++ read_app/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import home
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.home, name='home'),
+    #path('login/',auth_views.LoginView.as_view(template_name="pages/login.html"), name="login"),
+    #path('logout/',auth_views.LogoutView.as_view(next_page='/'),name='logout'),
 ]
