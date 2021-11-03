05afbfe37ed587960d46b6aa1e95bf0661db0642
anhkhoalongha <67829232+19520629@users.noreply.github.com>
2021-11-03 09:57:14 +0700

    register_raw

--- read_app/urls.py
+++ read_app/urls.py
@@ -8,4 +8,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register')
 ]
--- read_app/views.py
+++ read_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, HttpResponse
-
+from .forms import RegistrationForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 def home(request):
     return render(request, 'index.html',{})
+
+
+def register(request):
+    form=RegistrationForm()
+    if request.method=='POST':
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'dangki.html', {'form':form})
 
