43b42405431cc42bb7cac5f1eee50f914eb59c9a
Duy Đặng Ngọc Khánh <18520656@ms.uit.edu.vn>
2021-12-07 11:33:11 +0700

    updateuser

---
+++ read_app/forms.py
@@ -0,0 +1,75 @@
+from django import forms
+import re
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Comment, Account
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(label='Tài khoản', max_length=30)
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(label='Mật khẩu', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Nhập lại mật khẩu', widget=forms.PasswordInput())
+
+    def clean_password2(self):
+        if 'password1' in self.cleaned_data:
+            password1 = self.cleaned_data['password1']
+            password2 = self.cleaned_data['password2']
+            if password1 == password2 and password1:
+                return password2
+            raise forms.ValidationError("Mat khau khong hop le")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError("Tên tài khoản có kí tự đặc biệt")
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError("Tài khoản đã tồn tại")
+    def save(self):
+        User.objects.create_user(username=self.cleaned_data['username'], email=self.cleaned_data['email'], password=self.cleaned_data['password1'])
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class AccountUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ['hoten', 'gioitinh','user_avt','tuoi']
+
+
+class UserUpdateForm(forms.Form):
+    username = forms.CharField(label='Họ và Tên', max_length=30)
+    email = forms.CharField(label='Tuổi')
+    password1 = forms.CharField(label='Mật khẩu', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Nhập lại mật khẩu', widget=forms.PasswordInput())
+
+    def clean_password2(self):
+        if 'password1' in self.cleaned_data:
+            password1 = self.cleaned_data['password1']
+            password2 = self.cleaned_data['password2']
+            if password1 == password2 and password1:
+                return password2
+            raise forms.ValidationError("Mat khau khong hop le")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError("Tên tài khoản có kí tự đặc biệt")
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError("Tài khoản đã tồn tại")
+
+    def save(self):
+        User.objects.create_user( username=self.cleaned_data['username'], email=self.cleaned_data['email'], password=self.cleaned_data['password1'])
---
+++ read_app/views.py
@@ -0,0 +1,225 @@
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from .forms import RegistrationForm, CommentForm, UserUpdateForm, AccountUpdateForm
+from django.http import HttpResponseRedirect
+from .models import Sach, Nhac, Account, Comment, Favorite, User
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+import re
+import sys
+from django.contrib.auth.models import User
+
+
+
+# Create your views here.
+#function
+patterns = {
+    '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+    '[đ]': 'd',
+    '[èéẻẽẹêềếểễệ]': 'e',
+    '[ìíỉĩị]': 'i',
+    '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+    '[ùúủũụưừứửữự]': 'u',
+    '[ỳýỷỹỵ]': 'y'
+}
+
+def convert(text):
+    """
+    Convert from 'Tieng Viet co dau' thanh 'Tieng Viet khong dau'
+    text: input string to be converted
+    Return: string converted
+    """
+    output = text
+    for regex, replace in patterns.items():
+        output = re.sub(regex, replace, output)
+        # deal with upper case
+        output = re.sub(regex.upper(), replace.upper(), output)
+    return output
+#
+def register(request):
+    form=RegistrationForm()
+    if request.method=='POST':
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'dangki.html', {'form':form})
+
+def updateuser(request, id):
+    """ id = request.user.Account.id """
+    user = User.objects.get( id = id)
+    form=UserUpdateForm()
+    if request.method=='POST':
+        form=UserUpdateForm(request.POST or None, instance = user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'update-user.html', {'form':form, 'user': user})
+
+
+def home(request):
+    recommended_book = Sach.objects.all()
+    recommended_music = Nhac.objects.all()          
+    context={"titles":recommended_book[0:10], "nhac":recommended_music[0:5]}
+    return render(request, 'index.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+@login_required(login_url='/login/')
+def book_page(request):
+    recommended_book = Sach.objects.all()
+    context = {"titles": recommended_book}
+    return render(request, 'book-page.html', context)
+
+@login_required(login_url='/login/')
+def introbook(request, slug):
+    post = get_object_or_404(Sach, slug=slug)
+    recommended_book = Sach.objects.all()
+    comment_object = Comment.objects.filter(post=post)
+    comment_form = CommentForm(request.POST)
+
+    if request.method == 'POST':
+        if 'comment' in request.POST:
+            comment_form = CommentForm(request.POST or None)
+            if comment_form.is_valid():
+                body = request.POST.get('body')
+                comment = Comment.objects.create(post=post, user=request.user, body=body)
+                comment.save()
+                return HttpResponseRedirect(request.path_info)
+            else:
+                comment_form= CommentForm()
+        elif 'delete' in request.POST:
+            comment = Comment.objects.get(id=request.POST['comment_id'])
+            comment.delete()
+            return HttpResponseRedirect(request.path_info)
+        
+    
+
+    context = {"titles": recommended_book, "slug":slug,
+               "comments": comment_object,"comment_form": comment_form}
+    return render(request, 'intro-book.html', context)
+
+@login_required(login_url='/login/')
+def delete_comment(request):
+    post = get_object_or_404(Sach, slug=slug)
+    comment = Comment.objects.get(id=request.POST['comment_id'])
+    comment.delete()
+    return HttpResponseRedirect(request.path_info)
+
+
+@login_required(login_url='/login/')
+def readbook(request, slug, slug2):
+    book = get_object_or_404(Sach, slug=slug2)
+    book.book_luotxem += 1
+    book.save()
+    recommended_music = Nhac.objects.all()
+    recommended_book = Sach.objects.all()
+
+
+    context = {"titles": recommended_book, "slug":slug, "nhac": recommended_music}
+    book.save()
+    return render(request, 'read-book.html', context)
+
+
+
+def search(request):
+    dulieu=request.POST['search']
+    recommended_book1=[]
+    recommended_music1=[]
+    dulieu=(convert(dulieu)).lower().replace(" ", "")
+    #book
+    recommended_book = Sach.objects.all()
+    for item in recommended_book:
+        tensach=convert(item.book_tensach+item.book_tacgia.book_tacgia).lower().replace(" ", "")
+        if dulieu in tensach:
+            recommended_book1.append(item)
+            
+    #music
+    recommended_music = Nhac.objects.all()    
+    for item in recommended_music:
+        tennhac=convert(item.song_tenbaihat + item.song_casi_id.song_casi_ten).lower().replace(" ", "")
+        if dulieu in tennhac:
+            recommended_music1.append(item)
+    context = {"titles": recommended_book1, "nhac": recommended_music1, "slug":dulieu}
+    return render(request, 'search-page.html', context)
+
+
+@login_required(login_url='/login/')
+def search_book1(request):
+    dulieu='VN'
+    recommended_book = Sach.objects.filter(book_quocgia__icontains=dulieu)
+    context = {"titles": recommended_book, "slug":dulieu}
+    return render(request, 'search-page.html', context)
+
+
+@login_required(login_url='/login/')
+def search_book2(request):
+    dulieu='nuocngoai'
+    national=dulieu
+    recommended_book = Sach.objects.filter(book_quocgia__icontains=national)
+    context = {"titles": recommended_book,  "slug":dulieu}
+    return render(request, 'search-page.html', context)
+
+@login_required(login_url='/login/')
+def search_music1(request):
+    dulieu='VN'
+    recommended_music = Nhac.objects.filter(song_quocgia__icontains=dulieu)
+    context = {"nhac": recommended_music, "slug":dulieu}
+    return render(request, 'search-page.html', context)
+
+
+@login_required(login_url='/login/')
+def search_music2(request):
+    dulieu='nuocngoai'
+    recommended_music = Nhac.objects.filter(song_quocgia__icontains=dulieu)
+    context = {"nhac": recommended_music, "slug":dulieu}
+    return render(request, 'search-page.html', context)
+
+# @login_required(login_url='/login/')
+# def profile(request):
+#     if request.method == 'POST':
+#         u_form = UserUpdateForm(request.POST, instance=request.user)
+#         p_form = AccountUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+#         if u_form.is_valid() and p_form.is_valid():
+#             u_form.save()
+#             p_form.save()
+#     else:
+#         u_form = UserUpdateForm(instance=request.user)
+#         p_form = AccountUpdateForm(instance=request.user.profile)
+#         messages.success(request, f'Cập nhật thông tin thành công!')
+#         return redirect('profile')
+#
+#     context = {
+#         'u_form': u_form,
+#         'p_form': p_form
+#
+#     }
+#     return render(request, 'modal-user.html', context)
+
+
+# objects.get(id=request.user.id)
+
+
+@login_required(login_url='/login/')
+def edit_profile(request):
+    if request.method == 'POST':
+        # profile = Account.objects.get(user_ID=request.user.account.id)
+        user = User.objects.get(user_ID=request.user.account.id)
+        # profile.hoten = request.POST['hoten']
+        # profile.tuoi = request.POST['tuoi']
+        user.account.hoten = request.POST['hoten']
+        user.save()
+        user.account.hoten.save()
+        user.account.save()
+
+
+    return redirect('modal-user.html',{'profile':profile})
+
+
+
+
+
+
